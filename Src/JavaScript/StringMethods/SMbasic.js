ac6f1fc87a34bc18be7efa75f93134af5c8f73fc
Kalyan <venkatakalyan.ch@olivecrypto.com>
2025-06-12 22:44:39 +0530

    sm int

--- Src/JavaScript/StringMethods/SMbasic.js
+++ Src/JavaScript/StringMethods/SMbasic.js
@@ -30,9 +30,62 @@ const toTitleCase = ip => {
   return op;
 };
 
+const removeAllSpaces = ip => {
+  const op = ip.split(' ').join('');
+  return op;
+};
+const removeNonDigitChar = ip => {
+  const isNumber = c => /[0-9]/.test(c);
+  const op = ip
+    .split('')
+    .map(e => (isNumber(e) ? e : ''))
+    .join('');
+  return op;
+};
+
+const convertSnakeCaseToCamelCase = ip => {
+  const op = ip
+    .split('_')
+    .map((e, i) => (i == 0 ? e : e[0].toUpperCase() + e.slice(1)))
+    .join('');
+  return op;
+};
+
+const camelCaseToTitleCase = ip => {
+  const op = ip
+    .split('')
+    .map((e, i) => {
+      return i == 0 ? e.toUpperCase() : e == e.toUpperCase() ? ' ' + e : e;
+    })
+    .join('');
+  return op;
+};
+const removeDuplicateChar = ip => {
+  let uniChar = '';
+  ip.split('').forEach(e =>
+    !uniChar.includes(e) ? (uniChar = uniChar + e) : '',
+  );
+  return uniChar;
+};
+
+const removeDigitsAndSpecialCharacters = ip => {
+  const isChar = c => /[a-zA-Z]/.test(c);
+  const op = ip
+    .split('')
+    .map(e => (isChar(e) ? e : ''))
+    .join('');
+  return op;
+};
+
 export {
   reverseAString, //hello => olleh
   Palindrome, //Racecar => true
   vowelsCount, //Hello World => 3
   toTitleCase, //hello world from react => Hello World From React
+  removeAllSpaces, //" H e l l o " => "Hello"
+  removeNonDigitChar, // 123-456-7890 => 1234567890
+  convertSnakeCaseToCamelCase, //foo_bar_baz => fooBarBaz
+  camelCaseToTitleCase, //fooBarBaz => Foo Bar Baz
+  removeDuplicateChar, //aa$$2wwewwsd!@@!!000jfjdhyusj => a$2wesd!@0jfhyu
+  removeDigitsAndSpecialCharacters, //mix3dC4se! => mixdCse
 };
---
+++ Src/JavaScript/StringMethods/SMintermediate.js
@@ -0,0 +1,37 @@
+const compressStringByCountingRepeatedChars = ip => {
+  let res = '';
+  let count = 1;
+  ip.split('').forEach((e, i, a) => {
+    if (e == a[i + 1]) {
+      count++;
+    } else {
+      res = res + e + count.toString();
+      count = 1;
+    }
+  });
+  return res;
+};
+
+const addSpcBtwLtrsAndNums = ip => {
+  const isNum = c => /[0-9]/.test(c);
+  const isChar = c => /[a-zA-z]/.test(c);
+  const op = ip
+    .split('')
+    .map((e, i, a) => {
+      if (isChar(e)) {
+        if (isNum(a[i + 1])) {
+          return e + ' ';
+        } else return e;
+      } else if (isNum(e)) {
+        if (isChar(a[i + 1])) {
+          return e + ' ';
+        } else return e;
+      }
+    })
+    .join('');
+  return op;
+};
+export {
+  compressStringByCountingRepeatedChars, //aabbbcccc => a2b3c4
+  addSpcBtwLtrsAndNums,
+};
